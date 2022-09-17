949dd2a7e711553ecc5684620718d811aa450797
TheKrystof701 <krystofcejchan@seznam.cz>
2022-09-17 20:26:56 +0200

    Initial commit

---
+++ src/main/java/cz/krystofcejchan/air_quality_measurement/AqmApplication.java
@@ -0,0 +1,13 @@
+package cz.krystofcejchan.air_quality_measurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/krystofcejchan/air_quality_measurement/AqmApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.krystofcejchan.air_quality_measurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
