8a4274f7cbd69d1c967eee85aa8721dd99c7c27d
TheKrystof701 <krystofcejchan@seznam.cz>
2022-10-16 00:17:46 +0200

    back to old menu

--- Angular front-end AQM/AirData_front_ang/src/assets/js/main-page-main.js
+++ Angular front-end AQM/AirData_front_ang/src/assets/js/main-page-main.js
@@ -1,9 +1,14 @@
 (function ($) {
     "use strict";
 
+    var onLoaded = function () {
+
+    };
+
     var burgerMenu = function () {
 
         $('.js-colorlib-nav-toggle').on('click', function (event) {
+
             event.preventDefault();
             var $this = $(this);
 
@@ -21,28 +26,37 @@
     var mobileMenuOutsideClick = function () {
 
         $(document).click(function (e) {
-            console.log('ahoj')
-            var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
+            console.log(e)
+            const container = $("a#III");
+            const container2 = $("i#III");
+            const this_component_body = $("body#mainbody");
+            if ((container.is(e.target) || container2.is(e.target)) && (container.has(e.target).length === 0 || container2.has(e.target).length === 0)) {
 
-                if ($('body').hasClass('offcanvas')) {
+                if ($(this_component_body).hasClass('offcanvas')) {
 
-                    $('body').removeClass('offcanvas');
+                    //hides or shows the menu
+                    $(this_component_body).removeClass('offcanvas');
+
+                    // .js-colorlib-nav-toggle represents the icon which is used to show three lines or an X symbol
                     $('.js-colorlib-nav-toggle').removeClass('active');
 
+                }
+                else {
+                    $(this_component_body).addClass('offcanvas');
+                    $('.js-colorlib-nav-toggle').addClass('active');
                 }
 
             }
         });
 
-        $(window).scroll(function () {
-            if ($('body').hasClass('offcanvas')) {
-
-                $('body').removeClass('offcanvas');
-                $('.js-colorlib-nav-toggle').removeClass('active');
-
-            }
-        });
+        /*  $(window).scroll(function () {
+              if ($('body').hasClass('offcanvas')) {
+  
+                  $('body').removeClass('offcanvas');
+                  $('.js-colorlib-nav-toggle').removeClass('active');
+  
+              }
+          });*/
 
     };
     mobileMenuOutsideClick();
