c15ff533010ef82550616a91f021f666c6dfc267
TheKrystof701 <krystofcejchan@seznam.cz>
2022-10-15 18:24:56 +0200

    jquery added

--- Angular front-end AQM/AirData_front_ang/src/assets/js/main-page-main.js
+++ Angular front-end AQM/AirData_front_ang/src/assets/js/main-page-main.js
@@ -1,50 +1,50 @@
-(function($) {
+(function ($) {
+    "use strict";
 
-	"use strict";
+    var burgerMenu = function () {
 
-	var burgerMenu = function() {
+        $('.js-colorlib-nav-toggle').on('click', function (event) {
+            event.preventDefault();
+            var $this = $(this);
 
-		$('.js-colorlib-nav-toggle').on('click', function(event){
-			event.preventDefault();
-			var $this = $(this);
+            if ($('body').hasClass('offcanvas')) {
+                $this.removeClass('active');
+                $('body').removeClass('offcanvas');
+            } else {
+                $this.addClass('active');
+                $('body').addClass('offcanvas');
+            }
+        });
+    };
+    burgerMenu();
 
-			if ($('body').hasClass('offcanvas')) {
-				$this.removeClass('active');
-				$('body').removeClass('offcanvas');	
-			} else {
-				$this.addClass('active');
-				$('body').addClass('offcanvas');	
-			}
-		});
-	};
-	burgerMenu();
+    var mobileMenuOutsideClick = function () {
 
-	var mobileMenuOutsideClick = function() {
+        $(document).click(function (e) {
+            console.log('ahoj')
+            var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
 
-		$(document).click(function (e) {
-	    var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('offcanvas')) {
 
-	    	if ( $('body').hasClass('offcanvas') ) {
+                    $('body').removeClass('offcanvas');
+                    $('.js-colorlib-nav-toggle').removeClass('active');
 
-    			$('body').removeClass('offcanvas');
-    			$('.js-colorlib-nav-toggle').removeClass('active');
-			
-	    	}
-	    	
-	    }
-		});
+                }
 
-		$(window).scroll(function(){
-			if ( $('body').hasClass('offcanvas') ) {
+            }
+        });
 
-    			$('body').removeClass('offcanvas');
-    			$('.js-colorlib-nav-toggle').removeClass('active');
-			
-	    	}
-		});
+        $(window).scroll(function () {
+            if ($('body').hasClass('offcanvas')) {
 
-	};
-	mobileMenuOutsideClick();
+                $('body').removeClass('offcanvas');
+                $('.js-colorlib-nav-toggle').removeClass('active');
+
+            }
+        });
+
+    };
+    mobileMenuOutsideClick();
 
 })(jQuery);
