76a6a81566d9ddff36a3b8e100687e405e14a7b0
TheKrystof701 <krystofcejchan@seznam.cz>
2022-12-10 16:56:32 +0100

    first domain upload

--- Angular front-end AQM/AirData_front_ang/src/environments/environment.prod.ts
+++ Angular front-end AQM/AirData_front_ang/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:8080',
+  baseUrlAPI: 'http://localhost:8080/airdata/api',
+  APP_INITIALIZER_URL: 'http://localhost:8080/airdata/APP_INITIALIZER',
+  leaderboard: 'http://localhost:8080/airdata/leaderboard',
 };
